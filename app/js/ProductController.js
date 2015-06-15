48c5bf572d913796c8297862daf433c8910be667
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2015-06-15 13:46:24 +0200

    Productcontroller to external file

---
+++ app/js/ProductController.js
@@ -0,0 +1,72 @@
+
+app.controller('ProductsController',
+    ['$scope', '$timeout', '$routeParams', 'Products', 'BlockChain', 'Product', function($scope, $timeout, $routeParams, Products, BlockChain, Product) {
+
+        $scope.hideSpecial = false;
+        $scope.tagFilter = '';
+        $scope.products = [];
+        $scope.tags = [];
+        $scope.currencies = [];
+        $scope.sorting = {
+            by: ['special', 'price'],
+            desc: true
+        };
+        $scope.basket = {
+            sum: 0,
+            items: [],
+            checked: {}
+        };
+
+        $scope.testtest = $routeParams;
+        if ($routeParams.productId) {
+            $scope.product = Product.get({id: $routeParams.productId});
+        }
+        BlockChain.getLatestBTCPrice().then(function(data) {
+            console.log(data);
+
+            for (var key in data.data) {
+                if (key === 'length' || !data.data.hasOwnProperty(key)) continue;
+                $scope.currencies.push(key);
+            }
+            $scope.btcPrice = data.data.PLN.last;
+            $timeout(BlockChain.getLatestBTCPrice, 500);
+        });
+
+        $scope.isEditMode = true;
+        Products.list().then(function(response, dupa) {
+            $scope.products = response.data.results;
+        });
+
+        Product.get().$promise.then(function(response) {
+            var i, x;
+            for (i = 0; response.results.length > i; i++) {
+                for (x = 0; response.results[i].tags.length > x; x++) {
+                    if ($.inArray(response.results[i].tags[x], $scope.tags) < 0) {
+                        $scope.tags.push(response.results[i].tags[x]);
+                    }
+                }
+            }
+        });
+
+        $scope.filterTags = function(item, filter) {
+            if (filter != '') {
+                return ($.inArray(filter, item.tags) > -1);
+            }
+            return true;
+        };
+
+        $scope.addRemoveItemFromBasket = function(event, item) {
+            var key = 'id_' + item.id;
+            $scope.basket.checked[key] = $scope.basket.checked[key] || {};
+            if (event.target.checked && $scope.basket.items.indexOf(item) < 0) {
+                $scope.basket.items.push(item);
+                $scope.basket.checked[key].selected = true;
+                $scope.basket.sum += parseInt(item.price);
+            }
+            else {
+                $scope.basket.items.splice($scope.basket.items.indexOf(item), 1);
+                $scope.basket.checked[key].selected = false;
+                $scope.basket.sum -= parseInt(item.price);
+            }
+        };
+    }]);
